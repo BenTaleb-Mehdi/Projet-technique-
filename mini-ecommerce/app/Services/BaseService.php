91317f5c2737aaa0170c7c56ca2952f715670ed1
Mehdy <mehdibentaleb548@gmail.com>
2026-01-22 15:26:25 +0100

    push verstion

--- mini-ecommerce/app/Services/BaseService.php
+++ mini-ecommerce/app/Services/BaseService.php
@@ -1,12 +1,11 @@
 <?php
 
 namespace App\Services;
-
 use Illuminate\Database\Eloquent\Builder;
 
 class BaseService
 { 
-    public function applyPagination($query,$perPage = 10)
+    public function applyPagination($query,$perPage = 3)
     {
         return $query->paginate($perPage);
     }
